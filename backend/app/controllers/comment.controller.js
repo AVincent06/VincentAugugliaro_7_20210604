ba7391aba8fae17193fc8e6cf08bb171e2c26535
Vincent <vincent.augugliaro@orange.fr>
2021-07-11 18:31:08 +0200

    add the minimum structure to develop all remaining controllers

---
+++ backend/app/controllers/comment.controller.js
@@ -0,0 +1,28 @@
+const db = require("../models");
+const Comment = db.comments;
+const Op = db.Sequelize.Op;
+
+// créer un nouveau commentaire
+exports.create = (req, res) => {
+
+};
+
+// récupérer tous les commentaires du message par messageId
+exports.findAllByMessage = (req, res) => {
+
+};
+
+// récupérer un commentaire par id
+exports.findOne = (req, res) => {
+
+};
+
+// mettre à jour un commentaire par id
+exports.update = (req, res) => {
+
+};
+
+// effacer un commentaire par id
+exports.delete = (req, res) => {
+
+};
---
+++ backend/app/controllers/feeling.controller.js
@@ -0,0 +1,33 @@
+const db = require("../models");
+const Feeling = db.feelings;
+const Op = db.Sequelize.Op;
+
+// ajouter un like sur le message par id
+exports.addLike = (req, res) => {
+
+};
+
+// ajouter un dislike sur le message par id
+exports.addDislike = (req, res) => {
+
+};
+
+// récupérer tous les like pour le message par id
+exports.findAllLike = (req, res) => {
+
+};
+
+// récupérer tous les dislike pour le message par id
+exports.findAllDislike = (req, res) => {
+
+};
+
+// effacer un like sur le message par id
+exports.delLike = (req, res) => {
+
+};
+
+// effacer un dislike sur le message par id
+exports.delDislike = (req, res) => {
+
+};
