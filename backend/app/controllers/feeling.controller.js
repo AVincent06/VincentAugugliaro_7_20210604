35c42c4f6217a1528e0750e4df9da9ff9e1c8b73
Vincent <vincent.augugliaro@orange.fr>
2021-07-13 21:43:09 +0200

    add message and comment controller

--- backend/app/controllers/feeling.controller.js
+++ backend/app/controllers/feeling.controller.js
@@ -1,10 +1,30 @@
 const db = require("../models");
 const Feeling = db.feelings;
 const Op = db.Sequelize.Op;
+const LIKE = 1;
+const DISLIKE = 2;
 
 // ajouter un like sur le message par id
 exports.addLike = (req, res) => {
+    const id = req.params.id;
 
+    // Création du like
+    const feeling = {
+        MessageId: id,
+        UserId: req.body.user_id,
+        CategoryId: LIKE
+    }
+
+    // Sauvegarde du like dans la BDD
+    Feeling.create(feeling)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "erreur pendant la sauvegarde du like!"
+            });
+        });
 };
 
 // ajouter un dislike sur le message par id
--- backend/app/controllers/message.controller.js
+++ backend/app/controllers/message.controller.js
@@ -1,6 +1,7 @@
 const db = require("../models");
 const Message = db.messages;
 const Op = db.Sequelize.Op;
+const Sequelize = require("sequelize");
 
 // créer un nouveau message
 exports.create = (req, res) => {
@@ -33,30 +34,125 @@ exports.create = (req, res) => {
 
 // récupérer les nb derniers messages
 exports.findAllByAmount = (req, res) => {
-    // CONTINUER ICI
+    const nb = parseInt(req.params.nb, 10);
+
+    Message.findAll({
+        order: Sequelize.literal('createdAt DESC'),
+        limit: nb
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "erreur pendant la récupération des messages!"
+            });
+        });
 };
 
 // récupérer les messages jusqu'à date
 exports.findAllByDate = (req, res) => {
+    const date = req.params.date;
 
+    Message.findAll({
+        where: {
+            createdAt: {
+                [Op.lte]: date
+            }
+        }
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "erreur pendant la récupération des messages!"
+            });
+        });
 };
 
 // récupérer tous les message par userId
 exports.findAllByUser = (req, res) => {
+    const userId = req.params.userId;
 
+    Message.findAll({
+        where: {
+            UserId: {
+                [Op.eq]: userId
+            }
+        }
+    })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: err.message || "erreur pendant la récupération des messages!"
+            });
+        });
 };
 
 // récupérer un message par id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Message.findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `erreur pendant la récupération du message n° ${id}`
+            });
+        });
 };
 
 // mettre à jour un message par id
 exports.update = (req, res) => {
+    const id = req.params.id;
 
+    Message.update(req.body, {
+        where: {id: id}
+    })
+        .then( isUpdated => {
+            if(isUpdated) {
+                res.send({
+                    message: `Le message n°${id} a été mise à jour!`
+                });
+            } else {
+                res.send({
+                    message: `Le message n°${id} n'a pas pu être mise à jour!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `erreur pendant la mise à jour du message n° ${id}`
+            });
+        });
 };
 
 // effacer un message par id
 exports.delete = (req, res) => {
+    const id = req.params.id;
 
+    Message.destroy({
+        where: {id: id }
+    })
+        .then( isDeleted => {
+            if(isDeleted) {
+                res.send({
+                    message: `Le message n°${id} a été effacé!`
+                });
+            } else {
+                res.send({
+                    message: `Le message n°${id} n'a pas pu être effacé!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: `erreur pendant la suppression du message n° ${id}`
+            });
+        });
 };
