c5bd161613cb0c37fd8dfa3cc852dcaf5f34958f
Vincent <vincent.augugliaro@orange.fr>
2021-07-15 19:08:00 +0200

    add security and identify route and controller

--- backend/app/controllers/user.controller.js
+++ backend/app/controllers/user.controller.js
@@ -1,34 +1,88 @@
+require('dotenv').config();
+
+const passwordValidator = require('password-validator');//A2:2017 OWASP
+const bcrypt = require('bcrypt');                       //A2:2017 OWASP
+const emailValidator = require("email-validator");
+const jwt = require('jsonwebtoken');
+
 const db = require("../models");
 const User = db.users;
 const Op = db.Sequelize.Op;
 
+// Création d'un schéma pour le mot de passe
+const schema = new passwordValidator();
+schema
+    .is().min(12)
+    .is().max(64)
+    .has().uppercase()
+    .has().lowercase()
+    .has().digits()
+    .has().not().spaces();
+
 // créer un nouvel utilisateur
 exports.create = (req, res) => {
-    // Validation de la requète
-    if(!req.body.email || !req.body.password) {
-        res.status(400).send({ 
-            message: "Les champs email et password doivent être renseignés!" 
-        });
-        return;
-    }
+    const emailValidated = emailValidator.validate(req.body.email);
+    const passwordValidated = schema.validate(req.body.password);
+    
+    let feedback1 = '';
+    let feedback2 = '';
+    if(!emailValidated) feedback1 = 'Format email non valide. ';
+    if(!passwordValidated) feedback2 = 'Pour valider le password il faut entre 12 et 64 caractères sans espace et au moins une majuscule, une minuscule et un chiffre';
 
-    // Création d'un utilisateur
-    const user = {
-        email: req.body.email,
-        password: req.body.password,
-        is_admin: req.body.is_admin ? req.body.is_admin : false
+    if(emailValidated && passwordValidated) { 
+        bcrypt.hash(req.body.password, 10)
+            .then((hash) => {
+            // Création d'un utilisateur
+            const user = {
+                email: req.body.email,
+                password: hash,
+                is_admin: req.body.is_admin ? req.body.is_admin : false
+            }
+            // Sauvegarde de l'utilisateur dans la BDD
+            User.create(user)
+                .then(data => {
+                    res.send(data);
+                })
+                .catch(err => {
+                    res.status(500).send({
+                        message: err.message || "erreur pendant la création de l'utilisateur!"
+                    });
+                });
+            })
+            .catch((error) => res.status(500).json({ error }));
+    }else{
+        res.status(500).json({ error : feedback1+feedback2 });
     }
+};
 
-    // Sauvegarde de l'utilisateur dans la BDD
-    User.create(user)
-        .then(data => {
-            res.send(data);
+// identifier un utilisateur existant
+exports.identify = (req, res) => {
+    User.count({
+        where: {email : req.body.email}
+    })
+        .then((isExisted) => {
+            if(!isExisted) return res.status(401).json({ error : 'utilisateur non trouvé !'});
+            User.findOne({ // IL FAUT TOUT CORRIGER ICI
+                where: {email : req.body.email}
+            })
+                .then((data) => {   //console.log(data);
+                    bcrypt.compare(req.body.password, data.password)    //data.User.dataValues.password
+                        .then((valid) => {
+                            if(!valid) return res.status(401).json({ error : 'mot de passe incorrect !'});
+                            res.status(200).json({
+                                userId : data.id,
+                                token : jwt.sign(
+                                    { userId : data.id },
+                                    process.env.TOKEN_KEY,
+                                    { expiresIn : process.env.TOKEN_DURATION }
+                                )
+                            });
+                        })
+                        .catch((error) => res.status(500).json({ error: 'erreur pendant le bcrypt' }));
+                })
+                .catch((error) => res.status(500).json({ error: 'erreur pendant le findAll' }));
         })
-        .catch(err => {
-            res.status(500).send({
-                message: err.message || "erreur pendant la création de l'utilisateur!"
-            });
-        });
+        .catch((error) => res.status(500).json({ error: 'erreur pendant le count' }));
 };
 
 // récupérer tous les utilisateurs
--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,17 @@
 require('dotenv').config();
 
 const express = require("express");
-const cors = require("cors");
-
+const bodyParser = require('body-parser');
+const helmet = require('helmet');   //A7:2017 OWASP
+const path = require('path');
+// const cors = require("cors");
 const app = express();
 
-const corsOptions = {
-    origin: `http://localhost:${process.env.APP_PORT}`
-};
-
-app.use(cors(corsOptions));
-
+app.use(helmet());
 app.use(express.json());
-
 app.use(express.urlencoded({ extended: true }));
 
+/* Connexion à la base de données */
 const db = require("./app/models");
 db.sequelize.sync({ force: true }).then(() => { // {force: true} juste pour le développement
     console.log("BDD supprimée et synchronisée avec Sequelize");
@@ -27,6 +24,22 @@ db.sequelize.sync({ force: true }).then(() => { // {force: true} juste pour le d
         console.log("Table Categories initialisée!");
     });
 });
+
+// const corsOptions = {
+//     origin: `http://localhost:${process.env.APP_PORT}`
+// };
+// app.use(cors(corsOptions));
+
+/* Cross Origin Resource Sharing (CORS) */
+app.use((req, res, next) => {   //applies to all roads
+    res.setHeader('Access-Control-Allow-Origin', `http://${process.env.APP_HOST}:${process.env.APP_PORT}`);  //origin of access
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');    //authorized headers
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');    //authorized methods
+    next();
+});
+
+app.use(bodyParser.json());    // extracts the JSON object from the request
+app.use('/app/images', express.static(path.join(__dirname, 'images')));
 
 /* Déclaration des routes */
 require("./app/routes/user.routes")(app);
