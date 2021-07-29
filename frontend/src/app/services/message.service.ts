e8090a0611762e52bcf6837aece29077b77a3d1f
Vincent <vincent.augugliaro@orange.fr>
2021-07-29 17:20:12 +0200

    display news with new controller and model

--- frontend/src/app/services/message.service.ts
+++ frontend/src/app/services/message.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable, of, Subject } from 'rxjs';
-import { Message, Message_post } from '../models/message.model';
+import { Observable, of} from 'rxjs';
+import { Message, Message_news, Message_post } from '../models/message.model';
 import { AuthService } from './auth.service';
 
 @Injectable({
@@ -9,65 +9,7 @@ import { AuthService } from './auth.service';
 })
 export class MessageService {
 
-  messages: Message[] = [];
-  messagesSubject = new Subject<Message[]>();
-
-  constructor( private http: HttpClient, private authService: AuthService ) { 
-    this.messages = [
-      {
-        id: 0, 
-        profile_id: 1,
-        picture: 'https://www.letelegramme.fr/ar/imgproxy.php/images/2020/12/01/coucher-de-soleil-photographie-depuis-la-corniche-a_5419565_676x337p.jpg',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste similique nihil quidem commodi laboriosam in? A nemo provident nulla? Voluptates eius quia dolorem dolore temporibus et eligendi expedita reprehenderit commodi!',
-        usersLiked: [1,2],
-        usersDisliked: [3],
-        likes: 2,
-        dislikes: 1,
-        comments: 1,
-        create: '19-06-2021',
-        update: '',
-        profile_photo : 'https://material.angular.io/assets/img/examples/shiba1.jpg',
-        profile_nom: 'defamille',
-        profile_prenom: 'toto'
-      },
-      {
-        id: 1, 
-        profile_id: 2,
-        picture: '',
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste similique nihil quidem commodi laboriosam in? A nemo provident nulla? Voluptates eius quia dolorem dolore temporibus et eligendi expedita reprehenderit commodi! Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste similique nihil quidem commodi laboriosam in? A nemo provident nulla? Voluptates eius quia dolorem dolore temporibus et eligendi expedita reprehenderit commodi!',
-        usersLiked: [2],
-        usersDisliked: [1,3],
-        likes: 1,
-        dislikes: 2,
-        comments: 0,
-        create: '20-06-2021',
-        update: '21-06-2021',
-        profile_photo : '../../assets/images/profile.png',
-        profile_nom: 'defamille1',
-        profile_prenom: 'toto1'
-      },
-      {
-        id: 2, 
-        profile_id: 3,
-        picture: 'https://media.giphy.com/media/WTQcLBrPZt2sLvgIsk/giphy.gif',
-        text: '',
-        usersLiked: [1,3],
-        usersDisliked: [2],
-        likes: 2,
-        dislikes: 1,
-        comments: 0,
-        create: '21-06-2021',
-        update: '',
-        profile_photo : 'https://material.angular.io/assets/img/examples/shiba1.jpg',
-        profile_nom: 'defamille2',
-        profile_prenom: 'toto2'
-      }
-    ];
-   }
-
-  emitMessages(): void {
-    this.messagesSubject.next(this.messages);
-  }
+  constructor( private http: HttpClient, private authService: AuthService ) {}
 
   saveMessages(): void {
     // sauvegarde de this.Messages dans la BDD via l'API
@@ -82,27 +24,12 @@ export class MessageService {
     // SELECT Message.*, Profile.firstname, Profile.name, Profile.photo 
     // FROM Message INNER JOIN Profile ON Message.profile_id = Profile.id
     // WHERE Message.create IN(dans les 10 derniers);
-    this.emitMessages();
   }
 
-  getSingleMessage(id: number): Observable<Message> {
+  getSingleMessage(id: number){
     // chargement des données du message ciblé via l'API
-    return of(this.messages[id]);
   }
 
-  removeMessage(message: Message): void {
-    const messageIndexToRemove = this.messages.findIndex(
-      (messageElement) => {
-        if(messageElement === message) {
-          return true;
-        }
-        return false;
-      }
-    );
-    this.messages.splice(messageIndexToRemove, 1);
-    this.saveMessages();
-    this.emitMessages();
-  }
   /*---------------------- En accord avec le backend à partir de là ---------------------------*/
   postMessage(message: Message_post): Observable<any> {
     return this.http.post(
@@ -116,4 +43,29 @@ export class MessageService {
       }
     );
   }
+    
+  getNewsByAmount(nb: number): Observable<Message_news> {
+    return this.http.get<Message_news>(
+      `http://localhost:8080/api/messages/amount/${nb}/news`,
+      { 
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': 'Bearer ' + this.authService.getToken() 
+        }) 
+      }
+    );
+  }
+  
+  delMessage(id: number): Observable<any> {
+    return this.http.delete(
+      `http://localhost:8080/api/messages/${id}`,
+      { 
+        headers: new HttpHeaders({
+          'Content-Type':  'application/json',
+          'Authorization': 'Bearer ' + this.authService.getToken() 
+        }) 
+      }
+    );
+  }
+
 }
